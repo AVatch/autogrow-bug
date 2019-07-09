a69495d1e47de1c98e9b985d85204514f79e0692
Adrian Vatchinsky <adrian@uni.fyi>
2019-07-09 13:47:54 -0400

    ready

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomePage {
+  form: FormGroup = this.fb.group({
+    text: new FormControl('', [Validators.required, Validators.minLength(2)])
+  });
 
-  constructor() {}
+  constructor(public fb: FormBuilder) {}
 
+  onFormSubmit(evt: any) {
+    this.form.reset({
+      text: ''
+    });
+  }
 }
